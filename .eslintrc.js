c9a706bfe0163312b8b3dd18dbfc7e13eba4c5c8
Monika Smulko <monika.smulko@gmail.com>
2021-08-05 20:10:47 +0200

    Add babel and eslint with config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,33 @@
 module.exports = {
+  parser: "babel-eslint",
   env: {
     browser: true,
     commonjs: true,
     es2021: true,
+    node: true,
+    jest: true,
   },
-  extends: ["airbnb-base"],
+  extends: [
+    "airbnb-base",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2021,
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+    },
+    sourceType: "module",
   },
   rules: {
     quotes: ["error", "double", { allowTemplateLiterals: true }],
     "comma-dangle": ["error", "only-multiline"],
   },
-  plugins: ["html"],
+  plugins: ["html", "react", "react-hooks"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
 };
