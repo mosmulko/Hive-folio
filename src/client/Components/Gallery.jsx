890462a846b05f51b5f2586013ca493aaaed7d30
Monika Smulko <monika.smulko@gmail.com>
2021-08-05 21:54:28 +0200

    change export statement

--- src/client/Components/Gallery.jsx
+++ src/client/Components/Gallery.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function Gallery() {
+function Gallery() {
   return (
     <div>
       <h1>Hello from the other side</h1>
     </div>
   )
 }
+
+export default Gallery;
