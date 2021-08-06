397012ed2b72cf776547248179831e446a072511
Monika Smulko <monika.smulko@gmail.com>
2021-08-06 13:37:27 +0200

    Add img src prop to svg image tag

--- src/client/Components/Gallery.jsx
+++ src/client/Components/Gallery.jsx
@@ -7,19 +7,19 @@ function Gallery() {
     <>
       <section class='gallery__section gallery__section--column gallery__section--first'>
         <article class='gallery__card card'>
-          <CardImage />
+          <CardImage img='img1.jpg'/>
           <CardInfo />
         </article>
       </section>
       <section class='gallery__section gallery__section--column gallery__section--second'>
         <article class='gallery__card card'>
-          <CardImage />
+          <CardImage img='img2.jpg'/>
           <CardInfo />
         </article>
       </section>
       <section class='gallery__section gallery__section--fluid gallery__section--third'>
         <article class='gallery__card card'>
-          <CardImage />
+          <CardImage img='img3.jpg'/>
           <CardInfo />
         </article>
       </section>
