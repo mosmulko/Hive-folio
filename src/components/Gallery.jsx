23da661ec91448104d035387ce58a1aaea0a8f24
Monika Smulko <monika.smulko@gmail.com>
2021-08-09 22:16:02 +0200

    Change class to className

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,20 +5,20 @@ import CardInfo from './CardInfo';
 function Gallery() {
   return (
     <>
-      <section class='gallery__section gallery__section--column gallery__section--first'>
-        <article class='gallery__card card'>
+      <section className='gallery__section gallery__section--column gallery__section--first'>
+        <article className='gallery__card card'>
           <CardImage img='img1.jpg'/>
           <CardInfo />
         </article>
       </section>
-      <section class='gallery__section gallery__section--column gallery__section--second'>
-        <article class='gallery__card card'>
+      <section className='gallery__section gallery__section--column gallery__section--second'>
+        <article className='gallery__card card'>
           <CardImage img='img2.jpg'/>
           <CardInfo />
         </article>
       </section>
-      <section class='gallery__section gallery__section--fluid gallery__section--third'>
-        <article class='gallery__card card'>
+      <section className='gallery__section gallery__section--fluid gallery__section--third'>
+        <article className='gallery__card card'>
           <CardImage img='img3.jpg'/>
           <CardInfo />
         </article>
