1b94e3bba0fd4f6a12ed8a12fcaef6bbaaae8f93
Monika Smulko <monika.smulko@gmail.com>
2021-08-09 22:17:49 +0200

    Fix the img src path

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -4,7 +4,7 @@ const CardImage = ({img}) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 603.23 741.42">
         <polygon className="color--highlight" points="0 172.73 301.62 0 603.23 172.73 603.23 568.69 301.62 741.42 0 568.69 0 172.73"/>
-        <image className="card__img" xlinkHref={'/images/gallery/' + img} preserveAspectRatio="xMidYMin slice"></image>
+        <image className="card__img" xlinkHref={'../img/' + img} preserveAspectRatio="xMidYMin slice"></image>
       </svg>
   )
 }
